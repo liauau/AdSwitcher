9ac119684fb064117e9c31e2ee0ec8449d66682f
liauau <liauau2009@qq.com>
2017-05-27 15:13:22 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "[1] start deploy ..."
+echo -e "[2] modify ssh config"
+cp ~/.ssh/config ~/.ssh/config.bak
+cp ~/.ssh/config_vultr ~/.ssh/config
+echo -e "ssh config:"
+cat ~/.ssh/config
+
+echo -e "\n[3] start git push ..."
+git remote add production ssh://root@vultr:/opt/AdSwitcher
+git push production master
+echo "[4] git push completed."
+
+echo -e "\n[5] remove remote git reop."
+git remote remove production
+git remote -v
+echo -e "\n[6] restore ssh config"
+cp ~/.ssh/config.bak ~/.ssh/config
+echo "ssh config:"
+cat ~/.ssh/config
+echo -e "\n[7] deploy completed."
