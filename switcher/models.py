a23befbf003aceef6a79fca2207c57b9ef1016e4
liauau <liauau2009@qq.com>
2017-05-31 10:19:24 +0800

    modify database models

--- switcher/models.py
+++ switcher/models.py
@@ -11,21 +11,15 @@ class AppNode(models.Model):
         return 'pkg: %s' % self.pkg_name + ', owner: %s' % self.owner
 
 
-class Mix(models.Model):
-    default_platform = models.CharField(max_length=255)
-    app = models.OneToOneField('AppNode', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return 'default_platform: %s' % self.default_platform
-
-
-class PlacementMappings(models.Model):
+class Placement(models.Model):
     platform = models.CharField(max_length=255)
-    placement = models.CharField(max_length=255)
-    mix = models.ForeignKey('Mix', related_name='placement_mappings', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    sid = models.IntegerField()
+    extra = models.TextField()
+    app = models.ForeignKey('AppNode', related_name='placements', on_delete=models.CASCADE)
 
     def __str__(self):
-        return 'platform: %s' % self.platform + ', placement: %s' % self.placement
+        return 'name: %s' % self.name + ', platform: %s' % self.platform
 
 
 class Context(models.Model):
@@ -34,8 +28,17 @@ class Context(models.Model):
     version_name = models.CharField(max_length=255)
     version_code = models.IntegerField()
     label = models.CharField(max_length=255)
-    signatures = models.CharField(max_length=255)
+    signatures = models.TextField()
     app = models.ForeignKey('AppNode', related_name='context', on_delete=models.CASCADE)
 
     def __str__(self):
         return 'pkg_name: %s' % self.pkg_name
+
+
+class MetaData(models.Model):
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    context = models.ForeignKey('Context', related_name='meta_data', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'name: %s' % self.name + ', value: ' % self.value
--- switcher/serializer.py
+++ switcher/serializer.py
@@ -1,100 +1,90 @@
 from rest_framework import serializers
-from .models import AppNode, Context, Mix, PlacementMappings
+from .models import AppNode, Context, Placement, MetaData
 
 
-class PlacementMappingsSerializer(serializers.ModelSerializer):
-    # mix = serializers.ReadOnlyField(source='mix.id')
-
+class PlacementSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PlacementMappings
-        fields = ('id', 'platform', 'placement')
+        model = Placement
+        fields = ('id', 'name', 'platform', 'sid', 'extra')
         extra_kwargs = {'id': {'read_only': False, 'required': False}}
 
 
-class MixSerializer(serializers.ModelSerializer):
-    placement_mappings = PlacementMappingsSerializer(many=True)
-
+class MetaDataSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Mix
-        fields = ('default_platform', 'placement_mappings')
+        model = MetaData
+        fields = ('id', 'name', 'value')
+        extra_kwargs = {'id': {'read_only': False, 'required': False}}
 
 
 class ContextSerializer(serializers.ModelSerializer):
-    # app = serializers.ReadOnlyField(source='app.pkg_name')
+    meta_data = MetaDataSerializer(many=True)
 
     class Meta:
         model = Context
         fields = ('id', 'platform', 'pkg_name', 'version_name', 'version_code',
-                  'label', 'signatures')
+                  'label', 'signatures', 'meta_data')
         extra_kwargs = {'id': {'read_only': False, 'required': False}}
 
 
 class AppNodeSerializer(serializers.ModelSerializer):
     pkg_name = serializers.CharField(max_length=255)
-    owner = serializers.ReadOnlyField(source='owner.username')
     context = ContextSerializer(many=True)
-    mix = MixSerializer()
+    placements = PlacementSerializer(many=True)
 
     class Meta:
         model = AppNode
-        fields = ('pkg_name', 'owner', 'mix', 'context')
+        fields = ('pkg_name', 'placements', 'context')
 
     def create(self, validated_data):
-        mix_data = validated_data.pop('mix')
-        placement_mappings_data = mix_data.pop('placement_mappings')
+        placements_data = validated_data.pop('placements')
         context_data = validated_data.pop('context')
 
         app_node = AppNode.objects.create(**validated_data)
 
-        mix = Mix.objects.create(app=app_node, **mix_data)
+        for placement in placements_data:
+            Placement.objects.create(app=app_node, **placement)
 
         for contextItem in context_data:
-            Context.objects.create(app=app_node, **contextItem)
+            md_data = contextItem.pop('meta_data')
+            context = Context.objects.create(app=app_node, **contextItem)
+            for meta_item in md_data:
+                MetaData.objects.create(context=context, **meta_item)
 
-        for placement_mappings in placement_mappings_data:
-            PlacementMappings.objects.create(mix=mix, **placement_mappings)
         return app_node
 
     def update(self, instance, validated_data):
-        mix_data = validated_data.pop('mix')
-        placement_mappings_data = mix_data.pop('placement_mappings')
+        placements_data = validated_data.pop('placements')
         context_data = validated_data.pop('context')
-        mix = instance.mix
 
         self.update_appnode(instance, validated_data)
-        self.update_mix(mix, mix_data)
-        self.update_pm(mix, placement_mappings_data)
+        self.update_pm(instance, placements_data)
         self.update_context(instance, context_data)
         return instance
 
-    @staticmethod
-    def update_appnode(app_node, appnode_data):
-        app_node.pkg_name = appnode_data.get('pkg_name', app_node.pkg_name)
+    def update_appnode(self, app_node, appnode_data):
+        # do not update pkg_name since it is primary key of db.
+        # app_node.pkg_name = appnode_data.get('pkg_name', app_node.pkg_name)
         app_node.save()
 
-    @staticmethod
-    def update_mix(mix, mix_data):
-        mix.default_platform = mix_data.get('default_platform', mix.default_platform)
-        mix.save()
-
-    @staticmethod
-    def update_pm(mix, placement_mappings_data):
-        for single_pm in placement_mappings_data:
-            if single_pm:
-                expected_pm_id = single_pm.get('id', None)
+    def update_pm(self, app_node, placements_data):
+        for pm in placements_data:
+            if pm:
+                expected_pm_id = pm.get('id', None)
                 if expected_pm_id:
-                    old_pm_obj = PlacementMappings.objects.get(id=expected_pm_id, mix=mix)
-                    old_pm_obj.platform = single_pm.get('platform', old_pm_obj.platform)
-                    old_pm_obj.placement = single_pm.get('placement', old_pm_obj.placement)
+                    old_pm_obj = Placement.objects.get(id=expected_pm_id, app=app_node)
+                    old_pm_obj.name = pm.get('name', old_pm_obj.name)
+                    old_pm_obj.platform = pm.get('platform', old_pm_obj.platform)
+                    old_pm_obj.sid = pm.get('sid', old_pm_obj.sid)
+                    old_pm_obj.extra = pm.get('extra', old_pm_obj.extra)
                     old_pm_obj.save()
                 else:
-                    PlacementMappings.objects.create(mix=mix, **single_pm)
+                    Placement.objects.create(app=app_node, **pm)
 
-    @staticmethod
-    def update_context(app_node, context_data):
+    def update_context(self, app_node, context_data):
         for single_context in context_data:
             if single_context:
                 expected_context_id = single_context.get('id')
+                md_data = single_context.pop('meta_data')
                 if expected_context_id:
                     old_context_obj = Context.objects.get(id=expected_context_id, app=app_node)
                     old_context_obj.platform = single_context.get('platform', old_context_obj.platform)
@@ -104,5 +94,23 @@ class AppNodeSerializer(serializers.ModelSerializer):
                     old_context_obj.label = single_context.get('label', old_context_obj.label)
                     old_context_obj.signatures = single_context.get('signatures', old_context_obj.signatures)
                     old_context_obj.save()
+                    new_context_obj = old_context_obj
                 else:
-                    Context.objects.create(app=app_node, **context_data)
+                    new_context_obj = Context.objects.create(app=app_node, **single_context)
+
+                self.update_meta_data(new_context_obj, md_data)
+
+    def update_meta_data(self, context, md_data):
+        if not context:
+            return
+
+        for single_md in md_data:
+            if single_md:
+                expected_md_id = single_md.get('id')
+                if expected_md_id:
+                    old_md_obj = MetaData.objects.get(id=expected_md_id, context=context)
+                    old_md_obj.name = single_md.get('name', old_md_obj.name)
+                    old_md_obj.value = single_md.get('value', old_md_obj.value)
+                    old_md_obj.save()
+                else:
+                    MetaData.objects.create(context=context, **single_md)
