981f0c2e1f077d4e0fcf43d9ae685b33cbba4a65
liauau <liauau2009@qq.com>
2017-06-02 16:20:35 +0800

    handle DEBUG and RElEASE env

--- restart_uwsgi.sh
+++ restart_uwsgi.sh
@@ -1,5 +1,18 @@
-killall uwsgi
-cd /opt/AdSwitcher
+#!/bin/bash
+if [  $# -ne 1 ]; then
+    echo 'usage: restart_uwsgi.sh [port_number]'
+    exit
+fi
+
+UWSGI_PORT=$1
+PID=$(lsof -i:$UWSGI_PORT | tail -1 | awk '{ print $2}')
+kill -- -$(ps -o pgid= $PID | grep -o '[0-9]*')
+
+if [ $UWSGI_PORT = 8001]; then
+    cd /opt/AdSwitcher
+else
+    cd /opt/Debug_AdSwitcher
+fi
 pwd
 sleep 3s
 nohup uwsgi uwsgi.ini > uwsgi.log 2>&1 &
