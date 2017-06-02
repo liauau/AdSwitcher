b9acb806882e612399f394bf73ca4dd1b15bf702
liauau <liauau2009@qq.com>
2017-06-02 16:50:14 +0800

    handle DEBUG and RELEASE deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ $# -ne 1 ] || [ $1 != 'debug' ] && [ $1 != 'production' ]; then
+    echo 'usage: ./deploy.sh [debug|production]'
+    exit
+fi
+
+env=$1
 
 echo -e "[1] start deploy ..."
 echo "origin ssh config:"
@@ -9,15 +15,17 @@ echo -e "new ssh config:"
 cat ~/.ssh/config
 
 echo -e "\n[3] start git push ..."
-git remote add production ssh://root@vultr:/opt/AdSwitcher
-git push production master
+remote_repo='ssh://root@vultr:/opt/AdSwitcher'
+
+if [ $1 = 'debug' ]; then
+    remote_repo='ssh://root@vultr:/opt/Debug_AdSwitcher'
+fi
+echo 'push to '$remote_repo
+git push $remote_repo master
 echo "[4] git push completed."
 
-echo -e "\n[5] remove remote git reop."
-git remote remove production
-git remote -v
-echo -e "\n[6] restore ssh config"
+echo -e "\n[5] restore ssh config"
 cp ~/.ssh/config_default ~/.ssh/config
 echo "origin ssh config:"
 cat ~/.ssh/config
-echo -e "\n[7] deploy completed."
+echo -e "\n[6] deploy completed."
