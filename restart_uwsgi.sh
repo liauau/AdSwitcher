76ee773b83a8a532f676f8ab5db642c0a6fdb4bc
liauau <liauau2009@qq.com>
2017-06-02 16:22:30 +0800

    fix missing  bug

--- restart_uwsgi.sh
+++ restart_uwsgi.sh
@@ -8,7 +8,7 @@ UWSGI_PORT=$1
 PID=$(lsof -i:$UWSGI_PORT | tail -1 | awk '{ print $2}')
 kill -- -$(ps -o pgid= $PID | grep -o '[0-9]*')
 
-if [ $UWSGI_PORT = 8001]; then
+if [ $UWSGI_PORT = 8001 ]; then
     cd /opt/AdSwitcher
 else
     cd /opt/Debug_AdSwitcher
