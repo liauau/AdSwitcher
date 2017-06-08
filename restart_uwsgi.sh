c39227e41a9aa31b85cb384b37de9716131a58bf
liauau <liauau2009@qq.com>
2017-06-08 10:33:28 +0800

    check the port number validity when restart uwsgi

--- restart_uwsgi.sh
+++ restart_uwsgi.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+UWSGI_PORT=$1
+PID=$(lsof -i:$UWSGI_PORT | tail -1 | awk '{ print $2}')
+
 if [  $# -ne 1 ]; then
     echo 'usage: restart_uwsgi.sh [port_number]'
     exit
 fi
 
-UWSGI_PORT=$1
-PID=$(lsof -i:$UWSGI_PORT | tail -1 | awk '{ print $2}')
-kill -- -$(ps -o pgid= $PID | grep -o '[0-9]*')
-
 if [ $UWSGI_PORT = 8001 ]; then
     cd /opt/AdSwitcher
 else
-    cd /opt/Debug_AdSwitcher
+    echo 'The port number is not valid'
+    exit
 fi
+
+kill -- -$(ps -o pgid= $PID | grep -o '[0-9]*')
 pwd
 sleep 3s
 nohup uwsgi uwsgi.ini > uwsgi.log 2>&1 &
